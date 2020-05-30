9ea63cf3d9a4c4fd13ab89d1bef22e5927fd8dce
UraFIZ <kratser88@gmail.com>
2020-05-30 17:16:04 +0300

    working on resizness

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -4,7 +4,20 @@ import {createTable} from '@/components/table/table.template'
 export class Table extends ExcelComponent {
   static className = 'excel__table'
 
+  constructor($root) {
+    super($root, {
+      listeners: ['mousedown']
+    })
+  }
+
   toHTML() {
     return createTable(20)
   }
+
+  onMousedown = (event) => {
+    if(event.target.dataset.resize) {
+      console.log(event)
+    }
+  }
+
 }
--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -11,14 +11,20 @@ const CODES = {
   
   function toColumn(col) {
     return `
-      <div class="column">${col}</div>
+      <div class="column">${col}
+         <div class="col-resize" data-resize="col"></div>
+      </div>
     `
   }
   
   function createRow(index, content) {
+    const resize = index ? '<div class="row-resize" data-resize="row"></div>' : ''
     return `
       <div class="row">
-        <div class="row-info">${index ? index : ''}</div>
+        <div class="row-info">
+          ${index ? index : ''}
+          ${resize}
+        </div>
         <div class="row-data">${content}</div>
       </div>
     `
@@ -48,7 +54,6 @@ const CODES = {
   
       rows.push(createRow(i + 1, cells))
     }
-    console.log(rows)
     return rows.join('')
   }
   
